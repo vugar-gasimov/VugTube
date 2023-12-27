1ab0f04c7098e2baf838d8331402cab5b433cb77
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-27 11:50:46 +0200

    New data for video list , new files duration and time ago format, new component video grid item

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { CategoryPills } from "./components/CategoryPills";
-import { categories } from "./data/Pills";
+import { categories, videos } from "./data/Pills";
 import { Header } from "./layouts/Header";
+import { VideoGridItem } from "./components/VideoGridItem";
 
-function App() {
+export default function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return (
     <div className="max-h-screen flex flex-col">
@@ -18,10 +19,13 @@ function App() {
               onSelect={setSelectedCategory}
             />
           </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/VideoGridItem.tsx
@@ -0,0 +1,67 @@
+import { formatDuration } from "../utils/formatDuration";
+import { formatTimeAgo } from "../utils/formatTimeAgo";
+
+type VideoGridItemProps = {
+  id: string;
+  title: string;
+  channel: {
+    id: string;
+    name: string;
+    profileUrl: string;
+  };
+  views: number;
+  postedAt: Date;
+  duration: number;
+  thumbnailUrl: string;
+  videoUrl: string;
+};
+
+const VIEW_FORMATTER = new Intl.NumberFormat(undefined, {
+  notation: "compact",
+});
+
+export function VideoGridItem({
+  id,
+  title,
+  channel,
+  views,
+  postedAt,
+  duration,
+  thumbnailUrl,
+  videoUrl,
+}: VideoGridItemProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <a href={`/watch?v=${id}`} className="relative aspect-video">
+        <img
+          src={thumbnailUrl}
+          alt="Thumbnail"
+          className="block w-full h-full object-cover rounded-xl"
+        />
+        <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
+          {formatDuration(duration)}
+        </div>
+      </a>
+      <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+          <img
+            className="w-12 h-12 rounded-full"
+            src={channel.profileUrl}
+            alt="Profile link with rounded img"
+          />
+        </a>
+        <div className="flex flex-col">
+          <a href={`/watch?v=${id}`} className="font-bold">
+            {title}
+          </a>
+          <a href={`/@${channel.id}`} className="text-secondary-text text-sm">
+            {channel.name}
+          </a>
+          <div className="text-secondary-text text-sm">
+            {VIEW_FORMATTER.format(views)} Views • {formatTimeAgo(postedAt)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
